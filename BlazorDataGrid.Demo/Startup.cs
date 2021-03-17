42ff875744c9baede8859a432e10c796c3180bd7
Tim Purdum <tim@cedarrivertech.com>
2021-03-17 13:09:52 -0500

    Setup for deployment

--- BlazorDataGrid.Demo/Startup.cs
+++ BlazorDataGrid.Demo/Startup.cs
@@ -1,6 +1,7 @@
 using BlazorDataGrid.Demo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -38,6 +39,11 @@ namespace BlazorDataGrid.Demo
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
