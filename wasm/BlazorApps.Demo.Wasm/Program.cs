d7e05d67c37546d1d72c77ba390aa8e496b3aa27
Tim Purdum <tim@cedarrivertech.com>
2021-03-28 19:59:53 -0500

    fix multi-components in root component.

--- wasm/BlazorApps.Demo.Wasm/Program.cs
+++ wasm/BlazorApps.Demo.Wasm/Program.cs
@@ -1,9 +1,13 @@
 using BlazorApps.BlazorDataGrid.Demo;
+using BlazorApps.BlazorMusicKeyboard;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using System.Linq;
+using System.Text.Json;
 
 namespace BlazorApps.Demo.Wasm
 {
@@ -12,10 +16,23 @@ namespace BlazorApps.Demo.Wasm
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            
-            builder.RootComponents
-                .Add<Root>("#blazor-apps");
+            var jsRuntime = builder.Services.BuildServiceProvider().GetRequiredService<IJSRuntime>();
+            var module = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "/componentFinder.js");
+            var json = await module.InvokeAsync<JsonElement>("getBlazorComponents");
+            foreach(var id in json.EnumerateArray())
+            {
+                switch (id.ToString())
+                {
+                    case "blazor-data-grid":
+                        builder.RootComponents.Add<BdGridDemo>("#blazor-data-grid");
+                        break;
+                    case "blazor-music-keyboard":
+                        builder.RootComponents.Add<MusicKeyboardDemo>("#blazor-music-keyboard");
+                        break;       
+                }
+            }
 
+            if (!builder.RootComponents.Any()) return;
             builder.Services.AddScoped(
                 sp => new HttpClient {BaseAddress = new Uri(new Uri(builder.HostEnvironment.BaseAddress).Host)});
 
--- wasm/BlazorApps.Demo.Wasm/wwwroot/componentFinder.js
+++ wasm/BlazorApps.Demo.Wasm/wwwroot/componentFinder.js
@@ -1,3 +1,7 @@
 export function getComponentName() {
     return document.getElementById('blazor-apps').dataset.component;
 }
+
+export function getBlazorComponents() {
+    return Array.from(document.querySelectorAll('BlazorComponent')).map(a => a.id);
+}
